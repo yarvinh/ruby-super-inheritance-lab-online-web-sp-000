251ed0386ff25f4b06cdfb330eecee01978077b3
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 21:42:30 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,9 @@
+class Student
+  def hello
+    phrase =  "Hey there! I'm so excited to learn stuff."#,"How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  def raise_hand
+    phrase = 'Pick me!'
+    # 10.times { puts phrase }
+  end
+end
