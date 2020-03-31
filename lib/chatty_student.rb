edfddc3e1f1ad59d253e7a1018014ba0ec65af53
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 21:53:38 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,7 +4,7 @@ class ChattyStudent < Student
     puts phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    super
-    10.times { puts "Pick me!"}
+    10.times {super}
+    # 10.times { puts "Pick me!"}
   end
 end
