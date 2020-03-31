e76cf34b6beb5f41b8ada4be89884a5f849fd30f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 21:34:20 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,4 +2,8 @@ class ChattyStudent
   def hello
     puts "Hey there! I'm so excited to learn stuff.","How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
+  def raise_hand
+    phrase = 'Pick me!'
+    10.time.phrase
+  end
 end
